3b64bf77f04997e902319014e3d911546b058889
kenwilcox <wilcoxk@gmail.com>
2014-08-26 22:01:14 -0600

    Sends mail now

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+import os
+
 # Configuration
 timeout = 30
 url = "http://www.google.com"
 sleep = 10
+
+msg = "Site Down: " + url
+subject = "Site Down"
+msgto = os.environ.get('PYUP_TO')
+msgfrom = os.environ.get('PYUP_FROM')
+smtp = os.environ.get('PYUP_SMTP')
--- pyup.py
+++ pyup.py
@@ -3,22 +3,44 @@
 import urllib2
 import os.path
 import time
+import smtplib
+from email.mime.text import MIMEText
 import config
 
 def check_config():
-	ret = True
+	ret = False
 	missing = "Nothing"
 	conf = dir(config)
+	# This could be prettier
 	if not 'sleep' in conf:
-		ret = False
 		missing = 'sleep'
-	if not 'url' in conf:
-		ret = False
+	elif not 'url' in conf:
 		missing = 'url'
-	if not 'timeout' in conf:
-		ret = False
+	elif not 'timeout' in conf:
 		missing = 'timeout'
+	elif not 'msg' in conf:
+		missing = 'msg'
+	elif not 'subject' in conf:
+		missing = 'subject'
+	elif not 'msgto' in conf:
+		missing = 'msgto'
+	elif not 'msgfrom' in conf:
+		missing = 'msgfrom'
+	elif not 'smtp' in conf:
+		missing = 'smtp'
+	else:
+		ret = True
 	return ret, missing
+
+def notify_user():
+	msg = MIMEText(config.msg)
+	msg['Subject'] = config.subject
+	msg['From'] = config.msgfrom
+	msg['To'] = config.msgto
+
+	s = smtplib.SMTP(config.smtp)
+	s.sendmail(config.msgfrom, [config.msgto], msg.as_string())
+	s.quit()
 
 def main():
 	while(not os.path.isfile('./die')):
@@ -26,6 +48,8 @@ def main():
 		response = urllib2.urlopen(config.url, None, config.timeout)
 		#print dir(response)
 		print response.code, response.info()
+		if (not response.code == 404):
+			notify_user()
 		time.sleep(config.sleep)
 		#
 
